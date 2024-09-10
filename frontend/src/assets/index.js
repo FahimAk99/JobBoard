797a0bb9ed37b55620cd092d2f92b4419a38b876
FahimAk99 <127788221+FahimAk99@users.noreply.github.com>
2024-09-10 12:25:05 +0530

    Add files via upload

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,26 @@
+import NoProfile from "./user.png";
+import Google from "./google.png";
+import Linkedin from "./linkedin.png";
+import Youtube from "./youtube.png";
+import WhatsApp from "./whatsapp.png";
+import Twitter from "./twitter.png";
+import Instagram from "./instagram.png";
+import Spotify from "./spotify.png";
+import Facebook from "./facebook.png";
+import Office from "./office.png";
+import JobImg from "./job.jpg";
+
+
+export {
+  NoProfile,
+  Facebook,
+  WhatsApp,
+  Twitter,
+  Instagram,
+  Spotify,
+  Google,
+  Linkedin,
+  Youtube,
+  Office,
+  JobImg
+};
